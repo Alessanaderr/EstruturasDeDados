30e62c0c1c148cdb4d2624d7656129e1f22e8e8b
Alessa Nader <alessanader@outlook.com>
2025-09-12 17:53:50 -0300

    Adicionar EstruturasDeDados e Pilhas Estáticas

---
+++ EstruturasDeDados/Pilhas Estáticas/Empilhavel.java
@@ -0,0 +1,12 @@
+public interface Empilhavel {
+    // main methods
+    void push(Object value);       // C (Create)
+    Object peek();                 // R (Read)
+    void update(Object value);     // U (Update)
+    Object pop();                  // D (Delete)
+    
+    // helper methods
+    boolean isFull();
+    boolean isEmpty();
+    String printStack();
+}
